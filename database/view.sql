6f014eace5623dee15a84be1b63b52dafeb23713
Maherylgrd <maherylgrd@gmail.com>
2024-06-12 20:51:09 +0300

    liste Livraison livreur avec statut et gain
    tsy tao le view fa niala

--- database/view.sql
+++ database/view.sql
@@ -444,3 +444,43 @@ join Resto as r on r.id = vfl.id_resto
 join Client as c on c.id = vfl.id_client
 join Adresse as a1 on a1.id = vfl.recuperation
 join Adresse as a2 on a2.id = vfl.destination;
+
+CREATE OR REPLACE VIEW v_liste_livraison_livreur_jour AS
+SELECT
+    c.id AS id_commande,
+    l.id AS id_livreur,
+    a.nom AS adresse,
+    r.nom AS resto,
+    c.date AS date_commande,
+    lp.paye AS status_livraison
+FROM
+    Commande c
+JOIN
+    Livraison_payement_commande lp ON c.id = lp.id_commande
+JOIN
+    Livreur l ON lp.id_livreur = l.id
+JOIN
+    Commande_plat cp ON c.id = cp.id_commande
+JOIN
+    Plat p ON cp.id_plat = p.id
+JOIN
+    Resto r ON p.id_resto = r.id
+JOIN
+    Adresse a ON c.adresse = a.id
+GROUP BY
+    id_commande, resto;
+
+
+CREATE OR REPLACE VIEW v_livraison_livreur_avec_gain AS
+SELECT
+    vlllj.id_commande,
+    vlllj.id_livreur,
+    vlllj.date_commande,
+    vlllj.status_livraison,
+    COALESCE(g.montant, 0) AS gain
+FROM 
+    v_liste_livraison_livreur_jour vlllj
+LEFT JOIN
+    Payement g ON vlllj.id_commande  = g.id_commande
+GROUP BY
+    vlllj.id_commande;
